aa278fff55271832aadbc0cc9481c441e3011bb5
James Wu <ichangjwu@outlook.com>
2022-12-12 17:08:23 +0900

    Complete day 1 challenge

--- day_1.rb
+++ day_1.rb
@@ -1,3 +1,5 @@
+# Part One
+
 data_string = File.read('day 1 input.txt')
 elves_weights = data_string.split("\n\n")
 summed_weights = elves_weights.map do |weights|
@@ -5,3 +7,8 @@ summed_weights = elves_weights.map do |weights|
   weights_array.reduce(0) { |sum, weight| sum + weight.to_i }
 end
 puts summed_weights.max
+
+# Part Two
+
+descending_order_weights = summed_weights.sort { |a, b| b <=> a }
+puts descending_order_weights.slice(0, 3).sum
